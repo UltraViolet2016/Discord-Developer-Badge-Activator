171f981eb02d1308acf1f6c661022c4be83a0183
Dark <73867774+darkoce8@users.noreply.github.com>
2022-11-22 04:09:14 +1000

    Created Python Script & Updated Badge Command
    Python script does everything for you now and badge command shows the next steps

--- commands/badge.js
+++ commands/badge.js
@@ -1,11 +1,33 @@
-const { SlashCommandBuilder } = require('discord.js');
-const message = require('../embeds/general/embed.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('badge')
-		.setDescription('Run this command to get the discord developer badge.'),
+		.setDescription('Activate a slash command for the badge'),
 	async execute(interaction) {
-		await interaction.reply('Successfully ran bot command.');
+
+		const MessageEmbed = new EmbedBuilder()
+		.setColor('#2f3136')
+		.setDescription(`:white_check_mark: Activated slash command!
+						Please make this discord server a community server
+						
+						Once done, join this discord server
+						https://discord.gg/discord-developers
+						
+						Head over to 
+						https://discord.com/channels/613425648685547541/1040031099860045854
+						
+						Click the follow button and select this discord and choose any channel.
+						
+						Once completed, check 
+						https://discord.com/developers/active-developer
+						
+						Note: May take up to 24 hours to receive your badge.
+						
+						Bot Created By
+						https://github.com/darkoce8/
+						https://github.com/darkoce8/discord-developer-badge/`)
+
+		await interaction.reply({ embeds: [MessageEmbed] });
 	},
 };
---
+++ script.py
@@ -0,0 +1,64 @@
+import os
+import subprocess
+import sys
+import time
+import json
+
+output = subprocess.check_output('node -v', shell=True).decode("utf-8")
+
+print("""\
+  _____             _    _       _____  _                       _   ____            _                           _   _            _             
+ |  __ \           | |  ( )     |  __ \(_)                     | | |  _ \          | |                /\       | | (_)          | |            
+ | |  | | __ _ _ __| | _|/ ___  | |  | |_ ___  ___ ___  _ __ __| | | |_) | __ _  __| | __ _  ___     /  \   ___| |_ ___   ____ _| |_ ___  _ __ 
+ | |  | |/ _` | '__| |/ / / __| | |  | | / __|/ __/ _ \| '__/ _` | |  _ < / _` |/ _` |/ _` |/ _ \   / /\ \ / __| __| \ \ / / _` | __/ _ \| '__|
+ | |__| | (_| | |  |   <  \__ \ | |__| | \__ \ (_| (_) | | | (_| | | |_) | (_| | (_| | (_| |  __/  / ____ \ (__| |_| |\ V / (_| | || (_) | |   
+ |_____/ \__,_|_|  |_|\_\ |___/ |_____/|_|___/\___\___/|_|  \__,_| |____/ \__,_|\__,_|\__, |\___| /_/    \_\___|\__|_| \_/ \__,_|\__\___/|_|   
+                                                                                       __/ |                                                   
+                                                                                      |___/                                                  """)
+time.sleep(2)
+
+if (output[0] == 'v'): 
+    print('━━━━━━━━━━━━━━━━━━━━━', 'Node.js is installed.', sep='\n')
+    time.sleep(2)
+
+else: 
+    print('━━━━━━━━━━━━━━━━━━━━━', 'Node.js is not installed.', 'Install it via https://nodejs.org/en/download/ then run this script again.', sep='\n')
+    sys.exit()
+
+discordjs = os.path.exists('package.json')
+if (discordjs == False):
+    print('━━━━━━━━━━━━━━━━━━━━━', 'Installing discord.js onto your system.', sep='\n')
+    time.sleep(2)
+    os.system('npm i discord.js')
+else:
+    print('━━━━━━━━━━━━━━━━━━━━━', 'Skipping installation of discord.js as it is already installed', 'If this is an error, delete the package.json & package-lock.json', sep='\n')
+
+
+time.sleep(2)
+print('━━━━━━━━━━━━━━━━━━━━━', 'Head over to https://discord.com/developers/applications', 'Create a new application', sep='\n')
+time.sleep(2)
+client_id = input('Copy and paste Application ID: ')
+print('━━━━━━━━━━━━━━━━━━━━━', 'Click Bot and click Add Bot', sep='\n')
+time.sleep(2)
+discord_token = input('Click reset token and paste in your token: ')
+print('━━━━━━━━━━━━━━━━━━━━━', 'Create a discord server', sep='\n')
+time.sleep(2)
+guild_id = input('Right-click the server icon and Copy ID, paste the ID here: ')
+time.sleep(2)
+
+config = {
+	"token": discord_token,
+	"clientId": client_id,
+	"guildId": guild_id
+}
+
+with open('config1.json', 'w') as outfile:
+    outfile.write(json.dumps(config))
+
+
+print("Successfully created your config.json file.")
+time.sleep(2)
+os.system('node deploy-command.js')
+time.sleep(2)
+print("Type /badge in your discord server and follow the last instructions.")
+os.system('node .')
